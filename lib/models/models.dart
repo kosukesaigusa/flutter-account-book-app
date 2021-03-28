9b52d49c2b1089708aeb5ecd5c4b679825323e28
Kosuke Saigusa <saigusa758cloudy@gmail.com>
2021-03-28 15:46:52 +0900

    install sqfentity

---
+++ lib/models/models.dart
@@ -0,0 +1,139 @@
+import 'dart:convert';
+import 'package:http/http.dart' as http;
+import 'package:flutter/material.dart';
+import 'package:sqfentity/sqfentity.dart';
+import 'package:sqfentity_gen/sqfentity_gen.dart';
+part 'models.g.dart';
+
+@SqfEntityBuilder(dbModel)
+const dbModel = SqfEntityModel(
+  modelName: 'DbModel',
+  databaseName: 'account_book_app.db',
+  databaseTables: [
+    expense,
+    income,
+    expenseCategory,
+    incomeCategory,
+    fixedFee,
+    icon,
+    paymentCycle,
+  ],
+  bundledDatabasePath: null,
+);
+
+const expense = SqfEntityTable(
+  tableName: 'expense',
+  primaryKeyName: 'expense_id',
+  primaryKeyType: PrimaryKeyType.integer_auto_incremental,
+  useSoftDeleting: true,
+  fields: [
+    SqfEntityField('note', DbType.text),
+    SqfEntityFieldRelationship(
+      parentTable: expenseCategory,
+      fieldName: 'expense_category_id',
+      relationType: RelationType.ONE_TO_MANY,
+    ),
+    SqfEntityField('price', DbType.integer),
+    SqfEntityField('satisfaction', DbType.integer,
+        defaultValue: 3, minValue: 1, maxValue: 5),
+    SqfEntityField('year', DbType.integer),
+    SqfEntityField('month', DbType.integer),
+    SqfEntityField('date', DbType.integer),
+    SqfEntityField('created_at', DbType.datetime),
+  ],
+);
+
+const income = SqfEntityTable(
+  tableName: 'income',
+  primaryKeyName: 'income_id',
+  primaryKeyType: PrimaryKeyType.integer_auto_incremental,
+  useSoftDeleting: true,
+  fields: [
+    SqfEntityField('note', DbType.text),
+    SqfEntityFieldRelationship(
+      parentTable: incomeCategory,
+      fieldName: 'income_category_id',
+      relationType: RelationType.ONE_TO_MANY,
+    ),
+    SqfEntityField('price', DbType.integer),
+    SqfEntityField('year', DbType.integer),
+    SqfEntityField('month', DbType.integer),
+    SqfEntityField('date', DbType.integer),
+    SqfEntityField('created_at', DbType.datetime),
+  ],
+);
+
+const expenseCategory = SqfEntityTable(
+  tableName: 'expensecategory',
+  primaryKeyName: 'expense_category_id',
+  primaryKeyType: PrimaryKeyType.integer_auto_incremental,
+  useSoftDeleting: true,
+  fields: [
+    SqfEntityField('name', DbType.text),
+    SqfEntityField('budget', DbType.integer),
+    SqfEntityField('priority', DbType.integer),
+    SqfEntityFieldRelationship(
+      parentTable: icon,
+      fieldName: 'icon_id',
+      relationType: RelationType.ONE_TO_MANY,
+    ),
+    SqfEntityField('created_at', DbType.datetime),
+  ],
+);
+
+const incomeCategory = SqfEntityTable(
+  tableName: 'incomecategory',
+  primaryKeyName: 'income_category_id',
+  primaryKeyType: PrimaryKeyType.integer_auto_incremental,
+  useSoftDeleting: true,
+  fields: [
+    SqfEntityField('name', DbType.text),
+    SqfEntityField('priority', DbType.integer),
+    SqfEntityFieldRelationship(
+      parentTable: icon,
+      fieldName: 'icon_id',
+      relationType: RelationType.ONE_TO_MANY,
+    ),
+    SqfEntityField('created_at', DbType.datetime),
+  ],
+);
+
+const fixedFee = SqfEntityTable(
+  tableName: 'fixedfee',
+  primaryKeyName: 'fixed_fee_id',
+  primaryKeyType: PrimaryKeyType.integer_auto_incremental,
+  useSoftDeleting: true,
+  fields: [
+    SqfEntityField('name', DbType.text),
+    SqfEntityField('price', DbType.integer),
+    SqfEntityField('payment_cycle_id', DbType.integer),
+    SqfEntityFieldRelationship(
+      parentTable: paymentCycle,
+      fieldName: 'payment_cycle_id',
+      relationType: RelationType.ONE_TO_MANY,
+    ),
+    SqfEntityField('created_at', DbType.datetime),
+  ],
+);
+
+const icon = SqfEntityTable(
+  tableName: 'icon',
+  primaryKeyName: 'icon_id',
+  primaryKeyType: PrimaryKeyType.integer_auto_incremental,
+  useSoftDeleting: false,
+  fields: [
+    SqfEntityField('name', DbType.text),
+    SqfEntityField('priority', DbType.integer),
+  ],
+);
+
+const paymentCycle = SqfEntityTable(
+  tableName: 'paymentcycle',
+  primaryKeyName: 'payment_cycle_id',
+  primaryKeyType: PrimaryKeyType.integer_auto_incremental,
+  useSoftDeleting: false,
+  fields: [
+    SqfEntityField('month', DbType.integer),
+    SqfEntityField('priority', DbType.integer),
+  ],
+);
