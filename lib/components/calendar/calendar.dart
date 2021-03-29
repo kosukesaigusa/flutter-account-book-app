5c8f2ed38d558f49b4221a30922a47cc15ab8a26
Kosuke Saigusa <saigusa758cloudy@gmail.com>
2021-03-29 23:07:50 +0900

    feat: implement calendar widget with change notifier provider

--- lib/components/calendar/calendar.dart
+++ lib/components/calendar/calendar.dart
@@ -1,37 +1,58 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_account_book_app/common/datetime.dart';
 import 'package:flutter_account_book_app/common/text_style.dart';
+import 'package:flutter_account_book_app/components/calendar/calendar_model.dart';
+import 'package:provider/provider.dart';
 
 class CalendarWidget extends StatelessWidget {
-  CalendarWidget(this.year, this.month);
+  const CalendarWidget(this.year, this.month, this.day);
   final int year;
   final int month;
+  final int day;
   @override
-  Widget build(BuildContext context) => Column(
-        children: [
-          Row(
-            children: [
-              const Expanded(child: SizedBox()),
-              IconButton(
-                icon: const Icon(Icons.arrow_back_ios_outlined),
-                onPressed: () {},
-              ),
-              Text('$year年 $month月'),
-              IconButton(
-                icon: const Icon(Icons.arrow_forward_ios_outlined),
-                onPressed: () {},
-              ),
-              const Expanded(child: SizedBox()),
-            ],
-          ),
-          Row(
-            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-            children: weekdayRow(),
-          ),
-          calendar(year, month),
-        ],
+  Widget build(BuildContext context) => ChangeNotifierProvider(
+        create: (_) => CalendarWidgetModel(year, month, day),
+        child: Consumer<CalendarWidgetModel>(
+          builder: (context, model, child) {
+            return Column(
+              children: [
+                monthHandlingRow(context, model.year, model.month),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                  children: weekdayRow(),
+                ),
+                calendar(context, model.year, model.month, model.day),
+              ],
+            );
+          },
+        ),
       );
 
+  /// カレンダー上部の年月を表示・操作するウィジェット
+  Widget monthHandlingRow(BuildContext context, int year, int month) {
+    final model = Provider.of<CalendarWidgetModel>(context);
+    return Row(
+      children: [
+        const Expanded(child: SizedBox()),
+        IconButton(
+          icon: const Icon(Icons.arrow_back_ios_outlined),
+          onPressed: () {
+            model.showPreviousMonth();
+          },
+        ),
+        Text('$year年 $month月'),
+        IconButton(
+          icon: const Icon(Icons.arrow_forward_ios_outlined),
+          onPressed: () {
+            model.showNextMonth();
+          },
+        ),
+        const Expanded(child: SizedBox()),
+      ],
+    );
+  }
+
+  /// カレンダー上部の月〜日の曜日を表示するウィジェット
   List<Widget> weekdayRow() {
     final list = <Widget>[];
     for (var i = 0; i < 7; i++) {
@@ -47,7 +68,9 @@ class CalendarWidget extends StatelessWidget {
     return list;
   }
 
-  Widget calendar(int year, int month) {
+  /// カレンダーウィジェット
+  Widget calendar(BuildContext context, int year, int month, int day) {
+    final model = Provider.of<CalendarWidgetModel>(context);
     final weekDayOfFirstDay = DateTime(year, month, 1).weekday;
     final lastDay = getLastDay(year, month);
     final numRows = ((weekDayOfFirstDay - 1 + lastDay) / 7).ceil();
@@ -55,30 +78,39 @@ class CalendarWidget extends StatelessWidget {
     for (var i = 0; i < numRows; i++) {
       final weekRowChildren = <Widget>[];
       for (var j = 0; j < 7; j++) {
-        final isValid = 1 <= (i * 7 + j + 1 - (weekDayOfFirstDay - 1)) &&
-            (i * 7 + j + 1 - (weekDayOfFirstDay - 1)) <= lastDay;
+        final number = i * 7 + j + 1 - (weekDayOfFirstDay - 1);
+        final isValid = 1 <= number && number <= lastDay;
         final dateCell = Expanded(
           child: Container(
-            color: isValid ? Colors.transparent : Colors.grey[200],
+            color: isValid
+                ? day == number
+                    ? Colors.amber[200]
+                    : Colors.transparent
+                : Colors.grey[200],
             height: 80,
-            child: SingleChildScrollView(
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: isValid
-                    ? [
-                        Text('${i * 7 + j + 1 - (weekDayOfFirstDay - 1)}'),
-                        Text(
-                          '+ ¥2,000',
-                          style: calendarTotalIncomeTextStyle,
-                        ),
-                        Text(
-                          '- ¥1,500',
-                          style: calendarTotalExpenseTextStyle,
-                        ),
-                      ]
-                    : [],
-              ),
-            ),
+            child: isValid
+                ? InkWell(
+                    onTap: () {
+                      model.onDateCellTapped(number);
+                    },
+                    child: SingleChildScrollView(
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Text('$number'),
+                          Text(
+                            '+ ¥2,000',
+                            style: calendarTotalIncomeTextStyle,
+                          ),
+                          Text(
+                            '- ¥1,500',
+                            style: calendarTotalExpenseTextStyle,
+                          ),
+                        ],
+                      ),
+                    ),
+                  )
+                : const SizedBox(),
           ),
         );
         weekRowChildren.add(dateCell);
--- lib/screens/calendar_screen.dart
+++ lib/screens/calendar_screen.dart
@@ -2,10 +2,11 @@ import 'package:flutter/material.dart';
 import 'package:flutter_account_book_app/components/calendar/calendar.dart';
 
 class CalendarScreen extends StatelessWidget {
+  final today = DateTime.now();
   @override
   Widget build(BuildContext context) => Column(
         children: [
-          CalendarWidget(2021, 3),
+          CalendarWidget(today.year, today.month, today.day),
           // SingleChildScrollView(
           //   child: Text('支出一覧'),
           // ),
