c73c972dceea7bdee7e4de1918286cf1dda5995a
Kosuke Saigusa <saigusa758cloudy@gmail.com>
2021-03-30 22:43:22 +0900

    feat(category screen related): implement hard-coded category screen

--- lib/screens/category_screen.dart
+++ lib/screens/category_screen.dart
@@ -1,8 +1,22 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_account_book_app/components/category/category_card.dart';
 
 class CategoryScreen extends StatelessWidget {
   @override
-  Widget build(BuildContext context) => const Center(
-        child: Text('Category Page'),
+  Widget build(BuildContext context) => Container(
+        child: ListView.builder(
+          itemCount: cardList.length,
+          itemBuilder: (context, index) {
+            return cardList[index];
+          },
+        ),
       );
+
+  final cardList = <Widget>[
+    CategoryCardWidget(),
+    CategoryCardWidget(),
+    CategoryCardWidget(),
+    CategoryCardWidget(),
+    CategoryCardWidget(),
+  ];
 }
